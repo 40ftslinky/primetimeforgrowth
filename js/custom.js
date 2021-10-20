46d7bb9e51816bd7f54b8776b93f917b345864df
Jeremy Nicholson <40289122+40ftslinky@users.noreply.github.com>
2021-10-20 23:37:08 +1100

    minor updates

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+// add / remove field
+
+// jQuery(document).ready(function( $ ) {
+//     $( ".add_asset" ).click(function(){
+//        var $clone = $( ".assets_outer" ).first().clone();
+//        $clone.append( "<button class='remove_asset'>-</button>" );
+//        $clone.insertBefore( ".add-row" );
+//     });
+   
+//     $( "#form_02" ).on("click", ".remove_asset", function(){
+//        $(this).parent().remove();
+//     });
+//  });
+ jQuery(document).ready(function( $ ) {
+
+    $(".add_asset").click(function(event) {
+        event.preventDefault();
+        var $assets_label_html = $("label.file").html();
+        $(".assets_outer").append("<div class='asset_group'><label class='file'>"+$assets_label_html+"</label> <button class='remove_asset'>-</button></div>" );
+    });
+   
+    $( "#form_02" ).on("click", ".remove_asset", function(){
+       $(this).parent().remove();
+    });
+   
+});
+
+// need to add the file name of the upload to field after successful upload

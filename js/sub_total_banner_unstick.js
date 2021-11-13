bbe82d64cabcc11a191f6f2a8ac73488c1909ab6
Jeremy Nicholson <40289122+40ftslinky@users.noreply.github.com>
2021-11-13 18:43:42 +1100

    Update sub_total_banner_unstick.js

--- js/sub_total_banner_unstick.js
+++ js/sub_total_banner_unstick.js
@@ -1,17 +1,7 @@
 // add class to subtotal banner when footer reaches windowheight
 
 jQuery(document).ready(function( $ ) {
-    
-    // $(window).scroll(function(){
-    //     if ($(this).scrollTop() > 50) {
-    //        $('.box').addClass('box-hover');
-    //     } else {
-    //        $('.box').removeClass('box-hover');
-    //     }
-    // });
-
-
-    // $(window).on("scroll", function() {
+        
     $(window).scroll(function(){
         var docHeight = $(document).height();
         var winScrolled = window.innerHeight + $(window).scrollTop();
@@ -24,6 +14,8 @@ jQuery(document).ready(function( $ ) {
             $('.footer').css('margin-top','0');
         } else {
             $('.subtotal_banner_sect').removeClass('unfixed');
+            $('.footer').css('margin-top','5rem');
+
         }        
     });
 

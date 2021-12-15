6d930f4d569e20b77bae2ede20000a03f2151a60
Jeremy Nicholson <40289122+40ftslinky@users.noreply.github.com>
2021-12-15 17:09:54 +1100

    updates

--- js/add_class_is-active.js
+++ js/add_class_is-active.js
@@ -23,28 +23,45 @@ jQuery(document).ready(function( $ ) {
         }
     }); 
 
-    $(".menu-list .menu-link").on("click", function() {
+    // for mobile
+    $(".menu-list .menu-link").click(function() {
         $(this).next(".sub_menu-dropdown").slideToggle();
         $(this).toggleClass("is-active");
         
 		var count_active = 0;			
 		// loops through all a links in menu
-		var list_links = jQuery("#nav .menu-list .menu-item .menu-link");
+		var list_links = $("#nav .menu-list .menu-item .menu-link");
 		list_links.each(function(id, a_link) {
 			 // if this menus open? is- active
-			if (jQuery(a_link).hasClass("is-active")){
+			if ($(a_link).hasClass("is-active")){
                 // add to counter
                 count_active++;	 
 			}
 		});	   
 	   // if all are closed remove is-active class
 	   if(count_active==0){
-		   jQuery(".menu-icons").removeClass("is-active");
+		   $(".menu-icons").removeClass("is-active");
 	   } else {
 		// else add is-active class
-		 jQuery(".menu-icons").addClass("is-active");
+		 $(".menu-icons").addClass("is-active");
 	   }
+    });
+
+    $('.menu-link.has-sub').hover(function(){     
+        $(this).addClass('is-rotated');
+        $('.sub_menu-dropdown').addClass('is-visible');     
+ 
+      },function(){    
+        //  $('#responsive-menu-container').removeClass('is-active');     
+        $(this).removeClass('is-rotated'); 
+        $('.sub_menu-dropdown').removeClass('is-visible');     
+    
     });
 
 });
 
+// if ($( "#foo" ).hasClass('className')) {
+// 	$( "#foo" ).removeClass( 'className');
+// } else {
+//   $( "#foo" ).addClass( 'className');
+// }
--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,4 @@
-// add / remove field
 
-// jQuery(document).ready(function( $ ) {
-//     $( ".add_asset" ).click(function(){
-//        var $clone = $( ".assets_outer" ).first().clone();
-//        $clone.append( "<button class='remove_asset'>-</button>" );
-//        $clone.insertBefore( ".add-row" );
-//     });
-   
-//     $( "#form_02" ).on("click", ".remove_asset", function(){
-//        $(this).parent().remove();
-//     });
-//  });
  jQuery(document).ready(function( $ ) {
 
     $(".add_asset").click(function(event) {
@@ -25,12 +13,14 @@
    
 });
 
-// need to add the file name of the upload to field after successful upload
 
 
-// jQuery(document).ready(function( $ ) {
-//     $('[data-toggle="popover"]').popover({
-//        placement: 'top',
-//        trigger: 'hover'
-//     });
-//  });
+jQuery(document).ready(function( $ ) {
+    $('.sub_menu-link').hover(
+        // if ($( ".sub_menu-item_row" ).hasClass('is-hidden')) {
+        //     $( ".sub_menu-item_row" ).removeClass( 'is-hidden');
+        // } else {
+        //   $( ".sub_menu-item_row" ).addClass( 'is-hidden');
+        // }
+    )
+ });

eeaafc68b8068d9068b696a2632d8b067fe029fb
Jeremy Nicholson <40289122+40ftslinky@users.noreply.github.com>
2021-12-16 10:38:31 +1100

    js fix

--- js/add_class_is-active.js
+++ js/add_class_is-active.js
@@ -58,30 +58,7 @@ jQuery(document).ready(function( $ ) {
     
     });
 
-    // Show the first "tab" and hide the rest
-    $('.sub_menu-item .sub_menu-link:first-child').addClass('is-start');
-    $('.sub_menu-item_row').addClass('is-hidden').hide();
-    $('.sub_menu-item_row:first').addClass('is-shown').show();
-
-    // Click function
-    $('.sub_menu-link').mouseenter(function(){
-        // $('.sub_menu-item .sub_menu-link').removeClass('is-start');
-        // $(this).removeClass('is-start');
-        // track
-        // $(this).addClass('is-active');
-        $('.sub_menu-item_row').removeClass('is-hidden');
-        // $('.sub_menu-item_row').removeClass('is-shown');
-
-        // $(this).addClass('active');
-        $('.sub_menu-item_row').hide();
-        
-        //use href to locate & fade tab
-        var activeTab = $(this).find('.sub_menu-item_row');
-        $(activeTab).fadeIn();
-        $(activeTab).removeClass('is-hidden');
-
-        return false;
-    });
+    // tab-is-active function to go here
 
 });
 
